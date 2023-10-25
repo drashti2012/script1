d34cbccd58193b82d35368ed90df082cf669fe95
Drashti Mehta <143776631+drashti2012@users.noreply.github.com>
2023-10-25 14:20:20 +0530

    Add files via upload

---
+++ task3/script31.php
@@ -0,0 +1,8 @@
+<?php
+$a=array("a"=>"Disha","b"=>"Yashvi","c"=>"Jenny");
+echo array_search("Yashvi",$a);
+/*Output:
+b
+*/
+
+?>
---
+++ task3/script32.php
@@ -0,0 +1,9 @@
+<?php
+$a1=array("JIMJAM","HIDENSHICK");
+$a2=array("FANTACY","KRACKJACK");
+print_r(array_merge($a1,$a2));
+
+/*
+output:
+Array ( [0] => JIMJAM [1] => HIDENSHICK [2] => FANTACY [3] => KRACKJACK )*/
+?>
